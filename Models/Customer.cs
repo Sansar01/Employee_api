a3dea5e0caad03bb9c45c9de0a6f98e31544e057
Sansar Tiwari <89459552+Sansar01@users.noreply.github.com>
2024-03-20 12:59:39 +0530

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Employee_api.Models
+{
+    public class Customer
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        public string customerName { get; set; } = string.Empty;
+
+        public string customerMail { get; set; } = string.Empty;
+
+        public string customerGender { get; set; } = string.Empty;
+
+        public string customerAddress { get; set; } = string.Empty;
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string EmpId { get; set; } = string.Empty;
+
+        [BsonIgnoreIfNull]
+        public string? emp_Name { get; set; } = string.Empty;
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,25 +1,9 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Bson;
-
-namespace Employee_api.Models
+﻿namespace Employee_api.Models
 {
     public class User
     {
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; } = string.Empty;
+        public string Username { get; set; }
 
-        public string username { get; set; } = string.Empty;
-
-        public string usermail { get; set; } = string.Empty;
-
-        public string userGender { get; set; } = string.Empty;
-
-        public string userAddress { get; set; } = string.Empty;
-
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string EmpId { get; set; } = string.Empty;
-
-        [BsonIgnoreIfNull]
-        public string? employeeName { get; set; } = string.Empty;
+        public string Password { get; set; }
     }
 }
